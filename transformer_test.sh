4aabac80ded891d45722bf1daaa96e8ef822ecb1
Emil Poiesz <poiesz92@gmail.com>
2024-08-30 14:52:11 +0200

    Added easy way to look at results.

--- transformer_test.sh
+++ transformer_test.sh
@@ -1,7 +1,7 @@
 # Testing transformer model on the threesym case with {x0x1x2=1} is the unseen domain.
 # f(x) = x0x1 - 1.25x1x2 + 1.5 x0x2
 # f'(x) = x2 -1.25x0 + 1.5x1 is then the MD interpolator
-python3 main.py -seed 1 -task threesym -model transformer -lr 0.00002 -epochs 10 -batch-size 256 -opt adam
+#python3 main.py -seed 1 -task threesym -model transformer -lr 0.00002 -epochs 3 -batch-size 256 -opt adam
 
 # Testing the transformer model on the 2parity case with {(x0,x1) = (-1,-1)} is the unseen domain.
 # f(x) = x0x1
@@ -14,4 +14,4 @@ python3 main.py -seed 1 -task threesym -model transformer -lr 0.00002 -epochs 10
 #python3 main.py -seed 1 -task cyclic3dim15 -model transformer -lr 0.00002 -epochs 10 -batch-size 256 -opt adam
 
 # Testing the transformer model on the maj(x0x1x2) case.
-#python3 main.py -seed 1 -task maj3dim40freeze2 -model transformer -lr 0.00001 -epochs 10 -batch-size 256 -opt adam
+python3 main.py -seed 1 -task maj3dim40freeze2 -model transformer -lr 0.00001 -epochs 5 -batch-size 256 -opt adam
